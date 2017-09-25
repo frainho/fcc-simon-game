c47a68b193841c006c84b74682d44b48e83ec945
Filipe Neves <filipenrainho@gmail.com>
2017-09-25 21:02:34 +0200

    add sounds, swap divs with btns and add opacity to btns

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ var possibleColors = ['green', 'red', 'yellow', 'blue'];
 document.getElementById('visor').value = moveCounter;
 var color;
 var clickOrder = 0;
+var audio1 = new Audio('./sounds/simonSound1.mp3');
+var audio2 = new Audio('./sounds/simonSound2.mp3');
+var audio3 = new Audio('./sounds/simonSound3.mp3');
+var audio4 = new Audio('./sounds/simonSound4.mp3');
 
 //Starts the game
 document.getElementById("start").addEventListener("click", function () {
@@ -24,6 +28,7 @@ function runSeq() {
     let i = 0;
     var interval = setInterval(function () {
         lightOn(colorOrder[i]);
+        soundEngine(colorOrder[i]);
         i++;
         if (i >= colorOrder.length) {
             console.log(colorOrder);
@@ -39,9 +44,10 @@ function lightOn(btn) {
     }, 300);
 }
 
-
 document.getElementById("green").addEventListener("click", function () {
     clickOrder++;
+    soundEngine('green');
+    lightOn('green');
     if (this.id == colorOrder[clickOrder - 1]) {
         console.log('correct color');
     } else {
@@ -49,14 +55,17 @@ document.getElementById("green").addEventListener("click", function () {
         return;
     }
     if (clickOrder >= colorOrder.length) {
-        addColorSeq();
+        setTimeout(function() {
+            addColorSeq();
+        }, 1000);
     }
 
 });
 
 document.getElementById("red").addEventListener("click", function () {
-    console.log(colorOrder);
     clickOrder++;
+    soundEngine('red');
+    lightOn('red');
     if (this.id == colorOrder[clickOrder - 1]) {
         console.log('correct color');
     } else {
@@ -64,14 +73,16 @@ document.getElementById("red").addEventListener("click", function () {
         return;
     }
     if (clickOrder >= colorOrder.length) {
-        addColorSeq();
+        setTimeout(function() {
+            addColorSeq();
+        }, 1000);
     }
 });
 
 document.getElementById("yellow").addEventListener("click", function () {
-    console.log(colorOrder);
     clickOrder++;
-
+    soundEngine('yellow');
+    lightOn('yellow');
     if (this.id == colorOrder[clickOrder - 1]) {
         console.log('correct color');
     } else {
@@ -79,12 +90,15 @@ document.getElementById("yellow").addEventListener("click", function () {
         return;
     }
     if (clickOrder >= colorOrder.length) {
-        addColorSeq();
+        setTimeout(function() {
+            addColorSeq();
+        }, 1000);
     }
 });
 document.getElementById("blue").addEventListener("click", function () {
-    console.log(colorOrder);
     clickOrder++;
+    soundEngine('blue');
+    lightOn('blue');
     if (this.id == colorOrder[clickOrder - 1]) {
         console.log('correct color');
     } else {
@@ -92,6 +106,20 @@ document.getElementById("blue").addEventListener("click", function () {
         return;
     }
     if (clickOrder >= colorOrder.length) {
-        addColorSeq();
+        setTimeout(function() {
+            addColorSeq();
+        }, 1000);
     }
 });
+
+function soundEngine(sound) {
+    if (sound == 'red') {
+        audio1.play();
+    } else if (sound == 'blue') {
+        audio2.play();
+    } else if (sound == 'yellow') {
+        audio3.play();
+    } else {
+        audio4.play();
+    }
+} 
